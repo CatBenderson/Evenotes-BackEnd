30c48068dfb9e652c4bef8ecf0bcfa496f062a5d
CatBenderson <112489966+CatBenderson@users.noreply.github.com>
2022-12-09 17:58:54 -0600

    Update App.java

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -86,14 +86,13 @@ public class App {
             String x= DAOEvento.modificaEvento(ev);
             return x;
         });
-      
-        static int getHerokuAssignedPort() {
+    }
+    
+    static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
-    }
-        
     }
 }
